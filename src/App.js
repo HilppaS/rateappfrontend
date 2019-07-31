aa5e6321350e4b7eb1dfa778cda6c8b6809adcdb
Sanna <sanna.hilpas@gmail.com>
2019-07-31 08:55:10 +0300

    Feedin rakennetta muokattu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./component/Header";
 import Footer from './component/Footer';
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Starterpage from "./Main/Starterpage";
 
 
 function App() {
@@ -11,8 +12,7 @@ function App() {
         <BrowserRouter>
           <Header />
           <Switch>
-
-
+              <Route exact path="/starter" component={Starterpage} />
           </Switch>
           <Footer />
         </BrowserRouter>
